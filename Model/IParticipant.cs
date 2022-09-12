3d210e3c6083e45310073acc109acd429873f6d4
semmr1 <65629436+semmr1@users.noreply.github.com>
2022-09-12 18:02:35 +0200

    a commit

---
+++ Model/IParticipant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal interface IParticipant
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+
+    }
+}
